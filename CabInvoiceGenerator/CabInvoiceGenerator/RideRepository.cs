b92fcd6fec0f847ea2989b32ad1ea1556d640302
Arjunsing1997 <acchur1997@gmail.com>
2021-05-24 17:23:39 +0530

    UC1 Cab invoice Generator

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/RideRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        Dictionary<string, List<Ride>> userRide = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RideRepository"/> class.
+        /// Creating a constructor
+        /// </summary>
+        public RideRepository()
+        {
+            this.userRide = new Dictionary<string, List<Ride>>();
+        }
+
+        public void AddRide(string userId, Ride[] rides)
+        {
+            bool rideList = this.userRide.ContainsKey(userId);
+            try
+            {
+                if (!rideList)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    this.userRide.Add(userId, list);
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDES, "Invalid Rides");
+
+            }
+        }
+
+        public  Ride[] GetRide(string userId)
+        {
+            try
+            {
+                return this.userRide[userId].ToArray();
+            }
+            catch
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid User Id");
+            }
+        }
+    }
+}
