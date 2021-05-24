17f231c2ce216c415c8ec940d6409b886a3e9587
Arjunsing1997 <acchur1997@gmail.com>
2021-05-24 17:03:30 +0530

    Cab Invoice Generator Welcome Message

---
+++ CabInvoiceGenerator/CabInvoiceGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoiceGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CabInvoiceGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30d133bd-74ce-47b3-8e1f-36015b838c8f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
